1975beee6bf2e06172a2c90372cc240f5014dc16
folkaras <efolkaras@gmail.com>
2019-12-22 19:14:31 +0300

    change style

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -3,12 +3,12 @@
 </template>
 <style lang="scss">
 .load {
-	margin: 50px auto 0;
+  margin: 50px auto 0;
   width: 50px;
   height: 50px;
   border-radius: 100%;
-	border: 1px solid #000;
-  box-shadow: inset 4px 2px 0px 0px green;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  box-shadow: inset 4px 2px 0px 0px rgba(0, 0, 0, 0.6);
   animation: rotating 1s linear infinite;
 }
 @keyframes rotating {
