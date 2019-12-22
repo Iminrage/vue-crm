62d2cacf300956fdc24751517d4081d31a580bed
folkaras <efolkaras@gmail.com>
2019-12-22 14:24:02 +0300

    add pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="pagination">
+		<button class="pagination__arrow">&lt;</button>
+		<div class="pagination__dots">
+			<button class="pagination__dot" v-for="page in pages" :key="page">o</button>
+		</div>
+		<button class="pagination__arrow">&gt;</button>
+	</div>
+</template>
+<script>
+export default {
+	name: "Pagination",
+	props: {
+		pages: {
+			type: Number
+		}
+	}
+}
+</script>
