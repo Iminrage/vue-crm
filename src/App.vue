5329a27da49879e7d54126b6eca2e290ac5f91bb
folkaras <efolkaras@gmail.com>
2019-12-22 14:23:28 +0300

    add nav

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="app">
+    <nav class="navigation">
+      <ul class="navigation__list">
+        <li class="navigation__item">
+          <router-link class="navigation__link" to="/crm">CRM</router-link>
+        </li>
+        <li class="navigation__item">
+          <router-link class="navigation__link" to="/">Home</router-link>
+        </li>
+      </ul>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-  }
-}
+  name: "app",
+  components: {}
+};
 </script>
 
